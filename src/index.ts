143c1d1ac9979525b9487f6ecb4b1310dbdcdc83
EAlyce <msosjaahwwj@gmail.com>
2025-11-19 13:41:39 +0800

    Import logger for early initialization
    Added logger import for early initialization.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import { logger } from "@utils/logger"; // 引入 logger 以便尽早初始化
 import { login } from "@utils/loginManager";
 import { loadPlugins } from "@utils/pluginManager";
 import { patchMsgEdit } from "hook/listen";
