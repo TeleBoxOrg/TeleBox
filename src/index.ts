2b391fbdacd1f04280cdaa3587bb852e824bb885
TiaraBasori <mintabyss233@gmail.com>
2026-04-01 18:23:46 +0000

    refactor:通过运行时启动入口初始化
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import "dotenv/config";
 import { logger } from "@utils/logger"; // 引入 logger 以便尽早初始化
-import { login } from "@utils/loginManager";
-import { loadPlugins } from "@utils/pluginManager";
+import { startRuntime } from "@utils/runtimeManager";
 import { patchMsgEdit } from "hook/listen";
 import "./hook/patches/telegram.patch";
 
 // patchMsgEdit();
 
 async function run() {
-  await login();
-  await loadPlugins();
+  await startRuntime();
 }
 
 run();
