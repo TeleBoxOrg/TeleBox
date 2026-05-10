003a8b00d9545af959fdc4dab66a93549ac9fc71
TiaraBasori <mintabyss233@gmail.com>
2026-05-10 16:47:41 +0800

    refactor(utils): integrate GenerationContext into global client

--- src/utils/globalClient.ts
+++ src/utils/globalClient.ts
@@ -2,5 +2,8 @@ export {
   getGlobalClient,
   getCurrentRuntime,
   getCurrentGeneration,
+  getCurrentGenerationContext,
+  tryGetCurrentGenerationContext,
   isRuntimeTransitioning,
 } from "./runtimeManager";
+export type { GenerationContext } from "./generationContext";
